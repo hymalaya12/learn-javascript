b3d2950f179b0a21e0474ce61d9c2d3afdfbdcad
hymalaya12 <hymalaya12@naver.com>
2025-11-25 11:06:11 +0900

    first commit

---
+++ basic.js
@@ -0,0 +1,43 @@
+// console.log("Hello world");
+
+// 변수
+const milkCaramel = "빨간펜";
+let youtube = "후츠릿"; // 문자열
+
+youtube = "십오야";
+console.log("🚀 ~ youtube:", youtube);
+
+console.log("milkCaramel", milkCaramel);
+
+// milk caramel -> milkCaramel(카멜케이스)
+
+// 미션: 여러분의 나이를 age라는 변수에 담고 해당 변수를 console탭에 출력하는 코드를 작성하세요
+
+const age = 60; // 숫자
+console.log("🚀 ~ age:", age);
+
+/**
+ * 데이터타입
+ * 1. 문자열 (string)
+ * 2. 숫자 (number)
+ * 3. boolean
+ * 4. null/undefined - 값이 비어있음
+ */
+
+// 출입 가능 여부 판별
+const isComming = true;
+console.log("🚀 ~ isComming:", isComming, typeof isComming);
+console.log("age type", typeof age);
+
+// 함수
+
+// 함수 정의/선언
+function sayHello() {
+  // 실행할 로직들
+  console.log("Hello world");
+}
+
+// 함수 실행/호출
+sayHello();
+sayHello();
+sayHello();
